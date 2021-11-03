055660caaaef8b1787402ae7b17a61491d8cf607
SwetaaRawat <93546351+SwetaaRawat@users.noreply.github.com>
2021-11-03 14:27:20 +0530

    Add files via upload

---
+++ src/components/movies.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import {getMovies} from "../services/fakeMovieService";
+import ListGroup from "./common/listGroup";
+import MoviesTable from './moviesTable';
+import Pagination from './common/pagination';
+import { getGenres } from '../services/fakeGenreService';
+import {paginate} from '../utils/paginate';
+import moviesTable from './moviesTable';
+import _ from 'lodash';
+import { NavLink } from 'react-router-dom';
+import SearchBox from './searchBox';
+
+class Movies extends Component{
+    state = {
+        movies: [],
+        genres:[],
+        currentPage: 1,
+        pageSize: 4 ,
+        searchQuery: "",
+        selectedGenere: null,
+        sortColumn: {path: 'title' , order : 'asc'}
+    };
+
+    componentDidMount(){
+        const genres = [{ _id: "" ,name: "All Genres"}, ...getGenres()];
+
+         this.setState({movies:getMovies(), genres});
+    }
+
+   handleDelete = (movie) =>{
+       const movies = this.state.movies.filter(m => m._id !== movie._id);
+       this.setState({movies});
+   };
+ 
+   handleLike = movie =>{
+      const movies = [...this.state.movies];
+      const index = movies.indexOf(movie);
+      movies[index] = {...movies[index]};
+      movies[index].liked = !movies[index].liked;
+      this.setState({movies});
+
+   };
+
+   handlePageChange = page => {
+       this.setState({currentPage: page});
+
+   };
+
+   handleGenreSelect = genre => {
+       this.setState({selectedGenere: genre, searchQuery: "" ,currentPage: 1});
+   };
+
+   handleSearch = query => {
+       this.setState({searchedQuery: query,selectedGenere: null, currentPage: 1});
+   }
+
+   handleSort = sortColumn =>{
+       
+       this.setState({sortColumn });
+   };
+
+   getPagedData  = () =>{ 
+    const {
+        pageSize , 
+        currentPage, 
+        sortColumn, 
+        selectedGenere, 
+        searchQuery,
+        movies: allMovies
+    } = this.state;
+
+    let filtered = allMovies;
+    if(searchQuery)
+    filtered = allMovies.filter(m =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+        );
+
+    else if(selectedGenere && selectedGenere._id)
+     filtered = allMovies.filter(m => m.genre._id === selectedGenere._id);
+   
+
+   const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);   
+
+    return {totalCount: filtered.length, data: movies};
+   };
+
+    render(){
+        const { length: count} = this.state.movies;
+        const {pageSize , currentPage, sortColumn, searchQuery } = this.state;
+
+        if (count === 0)
+            return <p>There are no movies in the database.</p>;
+        
+     const  {totalCount, data:movies}= this.getPagedData();
+
+        return(
+        <div className ="row">
+            <div className="col-2">
+                <ListGroup 
+                items={this.state.genres} 
+                selectedItem={this.state.selectedGenere}
+                onItemSelect={this.handleGenreSelect}/>
+            </div>
+          
+            <div className="col">
+            
+           <NavLink to="/movies/new" >
+           <button className="btn btn-primary">New Movie</button>
+           </NavLink>
+
+            <p>Showing {totalCount} movies in the database.</p> 
+
+        <SearchBox value={searchQuery} onChange={this.handleSearch}/>
+
+        <MoviesTable 
+        movies={movies} 
+        sortColumn={sortColumn}
+        onLike={this.handleLike} 
+        onDelete={this.handleDelete}
+        onSort={this.handleSort}
+        />
+       <Pagination  
+       itemsCount={totalCount} 
+       pageSize={pageSize} 
+       currentPage={ currentPage}
+       onPageChange={this.handlePageChange}
+       />
+           
+
+          </div>
+          </div>
+        );
+    }
+}
+
+export default Movies;
